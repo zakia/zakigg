2f2d521a39bc27b7d68d8a2342f675f42c98f017
Adham <32067614+azakiio@users.noreply.github.com>
2025-03-24 15:04:51 -0400

    add new iframe

--- src/routes/collections/+page.svelte
+++ src/routes/collections/+page.svelte
@@ -9,3 +9,14 @@
 	referrerpolicy="strict-origin-when-cross-origin"
 	allowfullscreen
 ></iframe>
+<iframe
+	width="356"
+	height="633"
+  class="rounded-lg"
+	src="https://www.youtube.com/embed/mfCn1O1zZHI"
+	title="Reservation anxiety 🥘 #funny #shorts #skit #anxiety"
+	frameborder="0"
+	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+	referrerpolicy="strict-origin-when-cross-origin"
+	allowfullscreen
+></iframe>

8217f3c38cd508b9fc5ac214e88c85904509914d
Adham Zaki <32067614+azakiio@users.noreply.github.com>
2026-08-20 21:16:15 -0400

    chore: remove obsolete demos and fixtures

--- src/lib/crafts/carousels/meta.ts
+++
@@ -1,8 +0,0 @@
-import type { CraftMeta } from '$lib/crafts/types';
-
-export const meta: CraftMeta = {
-	title: 'Carousels',
-	description: 'My attempt at tackling this common design pattern.',
-	tags: ['web', 'fun'],
-	date: '2024-10-04'
-};
